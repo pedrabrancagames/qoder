b9a4cf3d281f644971947475c2225e4b186c9849
PedraBrancaGames <pedrabrancagames@gmail.com>
2025-09-02 08:09:51 -0300

    fix(visual-effects): Ajuste fino final na posição do feixe de prótons

--- visual-effects.js
+++ visual-effects.js
@@ -908,8 +908,8 @@ class ProtonParticle extends Particle {
 
         if (protonPackIcon) {
             const rect = protonPackIcon.getBoundingClientRect();
-            startX = (rect.left + rect.width / 2) - 30; // Ajustado para a esquerda
-            startY = rect.top - 15; // Ajustado para baixo
+            startX = (rect.left + rect.width / 2) - 50; // Ajustado para a esquerda
+            startY = rect.top - 10; // Ajustado para baixo
         }
 
         const targetX = window.innerWidth / 2;
